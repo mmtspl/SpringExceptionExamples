dc2735e10e886017eb74667551dc68f0eca3b9fc
mithunmajumdar <71630267+mithunmajumdar@users.noreply.github.com>
2022-10-15 20:35:57 +0530

    First Commit

---
+++ src/main/java/com/mmtspl/SpringExceptionExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.mmtspl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExceptionExamplesApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringExceptionExamplesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mmtspl/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.mmtspl.exception;
+
+import java.util.Date;  
+import org.springframework.http.HttpStatus;  
+import org.springframework.http.ResponseEntity;  
+import org.springframework.web.bind.annotation.ControllerAdvice;  
+import org.springframework.web.bind.annotation.ExceptionHandler;  
+import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.context.request.WebRequest;  
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler; 
+
+
+//defining exception handling for all the exceptions  
+
+@ControllerAdvice  
+@RestController  
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler  
+{  
+	@ExceptionHandler(Exception.class)  
+	//override method of ResponseEntityExceptionHandler class  
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request)  
+	{  
+		//creating exception response structure  
+		ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));  
+		//returning exception structure and specific status   
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);  
+	}  
+	
+	@ExceptionHandler(UserNotFoundException.class)  
+	//override method of ResponseEntityExceptionHandler class  
+	public final ResponseEntity<Object> handleUserNotFoundExceptions(UserNotFoundException ex, WebRequest request)  
+	{  
+		//creating exception response structure  
+		ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));  
+		//returning exception structure and Not Found status   
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);  
+	}     
+}  
