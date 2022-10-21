b88941ac194a134467e0eefa252831c44eeb35f1
mithunmajumdar <71630267+mithunmajumdar@users.noreply.github.com>
2022-10-21 14:03:06 +0530

    updated

--- src/main/java/com/mmtspl/exception/ExceptionResponse.java
+++ src/main/java/com/mmtspl/exception/ExceptionResponse.java
@@ -5,6 +5,7 @@ public class ExceptionResponse
 	private Date timestamp;
 	private String message;
 	private String details;
+	
 	public ExceptionResponse(Date timestamp, String message, String details) 
 	{
 		super();
